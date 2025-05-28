116bbe1f15da445bc5ecad18f14e0a5be5163cab
Emmanuel Osabutey <tettehosabutey49@gmail.com>
2025-05-28 14:44:43 +0100

    initial commit

---
+++ mypolygon.py
@@ -0,0 +1,9 @@
+import turtle
+
+# Create turtle and draw something
+bob = turtle.Turtle()
+bob.forward(100)  # Example action
+print(bob)  # Prints turtle object info
+
+# Keep window open (only call ONCE at the end)
+turtle.mainloop()
